b29ab902c8f6b32994b20a3c16f2f697f599e081
Marylyn Uzoukwu <muzoukwu@terpmail.umd.edu>
2024-12-14 12:30:15 -0500

    added a history.html and history.js file. Worked on code in index.js

--- index.js
+++ index.js
@@ -5,16 +5,20 @@ const bodyParser = require('body-parser')
 const app = express()
 const port = 3000
 app.use(bodyParser.json())
-app.use(express.static(__dirname + '/public'))
+app.use(express.static(__dirname + '/frontend'))
 
 
-const urlSupabase = ' https://xumavszsshyutvuenwey.supabase.co'
-const apikeySupabase = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inh1bWF2c3pzc2h5dXR2dWVud2V5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzM2MzgwNjEsImV4cCI6MjA0OTIxNDA2MX0.3OSp74M39oEzKGduiJVt-Rqd6L3E81IV-8SFogPBRjQ'
+const urlSupabase = 'https://xumavszsshyutvuenwey.supabase.co'
+const apikeySupabase ='eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inh1bWF2c3pzc2h5dXR2dWVud2V5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzM2MzgwNjEsImV4cCI6MjA0OTIxNDA2MX0.3OSp74M39oEzKGduiJVt-Rqd6L3E81IV-8SFogPBRjQ'
 // client is used to make it easy to connect and querry from the supabase database
 //allows us to get the data using commands
-
-
 const supabase = supabaseClient.createClient(urlSupabase, apikeySupabase);
+
+
+app.get('/',(req, res) => {
+    res.sendFile('frontend/projectfunctionality.html', { root: __dirname })
+})
+
 
 
 
