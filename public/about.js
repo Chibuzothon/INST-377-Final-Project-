be02294568093763a29068c63e13fa609c4fa862
fumeobi <115200802+fumeobi@users.noreply.github.com>
2024-12-14 15:45:07 -0500

    Added Advanced CSS animation (Screen Transition)

--- public/about.js
+++ public/about.js
@@ -4,4 +4,17 @@ document.addEventListener("DOMContentLoaded", () => {
       once: true, // Animation should happen only once
     });
   });
+
+  document.querySelectorAll("a").forEach((link) => {
+    link.addEventListener("click", (e) => {
+      e.preventDefault();
+      document.querySelector(".page-overlay").classList.add("active");
+  
+      // Delay navigation until overlay animation completes
+      setTimeout(() => {
+        window.location.href = e.target.href;
+      }, 500);
+    });
+  });
+  
   
